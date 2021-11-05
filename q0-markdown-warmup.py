753e70985545faf1d6874b3fa319221f19d863cc
Chittaranjan Velambur <chittaranjan@Chittaranjans-MacBook-Air.local>
2021-11-05 16:01:52 -0400

    PS6-Q3-part3: adds initial version of PS1 - Q0

---
+++ q0-markdown-warmup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ---
+
+# This is _question 0_ for [problem set 1](https://jbhender.github.io/Stats507/F21/ps/ps1.html) of [Stats 507](https://jbhender.github.io/Stats507/F21/)
+
+# > ### Question 0 is about Markdown
+
+# The next question is about the **Fibonnaci sequence**, ${F_{n}=F_{n−2}+F_{n−1}}$. In part *a* we will define a Python function `fib_rec()`.
+
+# Below is a ...
+
+# ### Level 3 Header
+
+# Next, we can make a bulleted list:
+#  - Item 1
+#    - detail 1
+#    - detail 2
+#  - Item 2
+
+# Finally, we can make an enumerated list:
+# 1. Item 1
+# 1. Item 2
+# 1. Item 3
+
+# ---
+
+
+
